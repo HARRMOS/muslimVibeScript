68fb1a3ab88e65fb1a399941dd944f52ae8ff763
HARRMOS <108294489+HARRMOS@users.noreply.github.com>
2025-04-02 19:43:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+from flask import Flask
 import boto3
 import datetime
-from botocore.signers import CloudFrontSigner
 
-# Remplace par tes clés OVH
-aws_access_key = "435971df068347ee833dedcb0e35ed3b"
-aws_secret_key = "687d0d8a3b9f40d18d2fcacca7216ae9"
+app = Flask(__name__)
 
-# Connexion au S3 OVH
-s3_client = boto3.client(
-    's3',
-    endpoint_url='https://s3.eu-west-par.io.cloud.ovh.net',
-    aws_access_key_id=aws_access_key,
-    aws_secret_access_key=aws_secret_key
-)
+@app.route('/')
+def generate_presigned_url():
+    aws_access_key = "435971df068347ee833dedcb0e35ed3b"
+    aws_secret_key = "687d0d8a3b9f40d18d2fcacca7216ae9"
 
-# Génération de l'URL pré-signée
-url = s3_client.generate_presigned_url(
-    'get_object',
-    Params={'Bucket': 'muslim.vibes', 'Key': 'message.mp4'},
-    ExpiresIn=3600  # Durée de validité (en secondes)
-)
+    s3_client = boto3.client(
+        's3',
+        endpoint_url='https://s3.eu-west-par.io.cloud.ovh.net',
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_key
+    )
 
-print(url)
+    url = s3_client.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': 'muslim.vibes', 'Key': 'message.mp4'},
+        ExpiresIn=3600
+    )
+
+    return url
+
+if __name__ == "__main__":
+    port = 5000  # Railway fournit un port dynamique, donc tu peux utiliser le port donné dans l'environnement
+    app.run(host="0.0.0.0", port=port)
